d6d53c08b827b26a0b1c8850b411e77c070a5dce
Waihon Yew <yewwaihon@gmail.com>
2021-01-02 15:49:37 +0000

    Add a test for premium of non-smoking male

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -27,4 +27,16 @@ class PremiumCalculatorTest < Minitest::Test
     expected = 107
     assert_equal expected, calculator.premium_amount
   end
+
+  def test_premium_male_non_smoker
+    calculator = PremiumCalculator.new(
+      gender: "M",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "N",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01")
+    )
+    expected = 108
+    assert_equal expected, calculator.premium_amount
+  end
 end
