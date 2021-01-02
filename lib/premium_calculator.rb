c732d88fd9d4286fff4da8e3be3d675a70bfd2e3
Waihon Yew <yewwaihon@gmail.com>
2021-01-02 15:05:48 +0000

    Add an initialize method

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -1,2 +1,10 @@
 class PremiumCalculator
+  def initialize(gender:, date_of_birth:, smoking_status:,
+                 coverage_amount:, effective_date: Date.today)
+    @gender = gender
+    @date_of_birth = date_of_birth
+    @smoking_status = smoking_status
+    @coverage_amount = coverage_amount
+    @effective_date = effective_date
+  end
 end
