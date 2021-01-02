ee00ba9937b71248ca53cd96d2b20daf8853684e
Waihon Yew <yewwaihon@gmail.com>
2021-01-02 15:02:42 +0000

    Add a test for premium of non-smoking female

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -1,5 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'date'
+require_relative '../lib/premium_calculator'
 
 class PremiumCalculatorTest < Minitest::Test
+  def test_premium_female_non_smoker
+    calculator = PremiumCalculator.new(
+      gender: "F",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "N",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01")
+    )
+    expected = 80
+    assert_equal expected, calculator.premium_amount
+  end
 end
