a7b6fdaa65ef3dc8578f19204048718fefc00875
Waihon Yew <yewwaihon@gmail.com>
2021-01-03 13:57:01 +0000

    Add birthday? predicate method

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -45,4 +45,14 @@ class PremiumCalculator
 end
 
 class Age
+  attr_reader :date_of_birth, :now
+
+  def initialize(date_of_birth:, now: Date.today)
+    @date_of_birth = date_of_birth
+    @now = now
+  end
+
+  def birthday?
+    date_of_birth.month == now.month && date_of_birth.day == now.day
+  end
 end
