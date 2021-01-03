b65c2b0bf0e0bffb627a05c570da54430c9fad0c
Waihon Yew <yewwaihon@gmail.com>
2021-01-03 15:40:08 +0000

    Align second argument to the first one

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -80,91 +80,91 @@ end
 class AgeTest < Minitest::Test
   def test_birthday_on_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2021-01-01"))
+                  now: Date.parse("2021-01-01"))
     assert_equal true, age.birthday?
   end
 
   def test_birthday_on_one_day_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2020-12-31"))
+                  now: Date.parse("2020-12-31"))
     assert_equal false, age.birthday?
   end
 
   def test_birthday_on_one_day_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2021-01-02"))
+                  now: Date.parse("2021-01-02"))
     assert_equal false, age.birthday?
   end
 
   def test_birthday_on_same_day_one_month_before
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2020-12-01"))
+                  now: Date.parse("2020-12-01"))
     assert_equal false, age.birthday?
   end
 
   def test_birthday_on_same_day_one_month_after
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2021-02-01"))
+                  now: Date.parse("2021-02-01"))
     assert_equal false, age.birthday?
   end
 
   def test_birthday_passed_on_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-01"),
-                 now: Date.parse("2021-01-01"))
+                  now: Date.parse("2021-01-01"))
     assert_equal false, age.birthday_passed?
   end
 
   def test_birthday_passed_on_one_day_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-18"),
-                 now: Date.parse("2021-01-17"))
+                  now: Date.parse("2021-01-17"))
     assert_equal false, age.birthday_passed?
   end
 
   def test_birthday_passed_on_one_day_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-01-18"),
-                 now: Date.parse("2021-01-19"))
+                  now: Date.parse("2021-01-19"))
     assert_equal true, age.birthday_passed?
   end
 
   def test_birthday_passed_on_previous_month_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-02-19"))
+                  now: Date.parse("2021-02-19"))
     assert_equal false, age.birthday_passed?
   end
 
   def test_birthday_passed_on_next_month_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-04-17"))
+                  now: Date.parse("2021-04-17"))
     assert_equal true, age.birthday_passed?
   end
 
   def test_current_age_on_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-03-18"))
+                  now: Date.parse("2021-03-18"))
     assert_equal 18, age.current
   end
 
   def test_current_age_on_one_day_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-03-17"))
+                  now: Date.parse("2021-03-17"))
     assert_equal 17, age.current
   end
 
   def test_current_age_on_one_day_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-03-19"))
+                  now: Date.parse("2021-03-19"))
     assert_equal 18, age.current
   end
 
   def test_current_age_on_previous_month_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-02-19"))
+                  now: Date.parse("2021-02-19"))
     assert_equal 17, age.current
   end
 
   def test_current_age_on_next_month_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
-                 now: Date.parse("2021-04-17"))
+                  now: Date.parse("2021-04-17"))
     assert_equal 18, age.current
   end
 end
