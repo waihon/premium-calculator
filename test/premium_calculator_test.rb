c70b7b1a194c178dcb6f2b7045a7ced26345e69c
Waihon Yew <yewwaihon@gmail.com>
2021-01-03 14:22:43 +0000

    Test birthday_passed? on birthday

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -107,4 +107,10 @@ class AgeTest < Minitest::Test
                  now: Date.parse("2021-02-01"))
     assert_equal false, age.birthday?
   end
+
+  def test_birthday_passed_on_birthday
+    age = Age.new(date_of_birth: Date.parse("2003-01-01"),
+                 now: Date.parse("2021-01-01"))
+    assert_equal false, age.birthday_passed?
+  end
 end
