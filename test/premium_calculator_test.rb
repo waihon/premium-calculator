2754226bcfc52b8fb4c870425394292d48ec9a55
Waihon Yew <yewwaihon@gmail.com>
2021-01-05 16:06:57 +0000

    Inject a quote object to PremiumCalculator for remaining tests

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -25,60 +25,100 @@ class PremiumCalculatorTest < Minitest::Test
   end
 
   def test_premium_female_smoker
-    calculator = PremiumCalculator.new(
+    quote = Quote.new(
       gender: "F",
       date_of_birth: Date.parse("2003-01-01"),
       smoking_status: "S",
       coverage_amount: 100_000,
       effective_date: Date.parse("2021-01-01")
+    )
+    calculator = PremiumCalculator.new(
+      gender: "F",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "S",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01"),
+      quote: quote
     )
     expected = 107
     assert_equal expected, calculator.premium_amount
   end
 
   def test_premium_male_non_smoker
-    calculator = PremiumCalculator.new(
+    quote = Quote.new(
       gender: "M",
       date_of_birth: Date.parse("2003-01-01"),
       smoking_status: "N",
       coverage_amount: 100_000,
       effective_date: Date.parse("2021-01-01")
+    )
+    calculator = PremiumCalculator.new(
+      gender: "M",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "N",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01"),
+      quote: quote
     )
     expected = 108
     assert_equal expected, calculator.premium_amount
   end
 
   def test_premium_male_smoker
-    calculator = PremiumCalculator.new(
+    quote = Quote.new(
       gender: "M",
       date_of_birth: Date.parse("2003-01-01"),
       smoking_status: "S",
       coverage_amount: 100_000,
       effective_date: Date.parse("2021-01-01")
     )
+    calculator = PremiumCalculator.new(
+      gender: "M",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "S",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01"),
+      quote: quote
+    )
     expected = 153
     assert_equal expected, calculator.premium_amount
   end
 
   def test_premium_higher_coverage_amount
-    calculator = PremiumCalculator.new(
+    quote = Quote.new(
       gender: "F",
       date_of_birth: Date.parse("2003-01-01"),
       smoking_status: "N",
       coverage_amount: 150_000,
       effective_date: Date.parse("2021-01-01")
     )
+    calculator = PremiumCalculator.new(
+      gender: "F",
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: "N",
+      coverage_amount: 150_000,
+      effective_date: Date.parse("2021-01-01"),
+      quote: quote
+    )
     expected = 120
     assert_equal expected, calculator.premium_amount
   end
 
   def test_premium_earlier_date_of_birth
-    calculator = PremiumCalculator.new(
+    quote = Quote.new(
       gender: "F",
       date_of_birth: Date.parse("1961-01-01"),
       smoking_status: "N",
       coverage_amount: 100_000,
       effective_date: Date.parse("2021-01-01")
+    )
+    calculator = PremiumCalculator.new(
+      gender: "F",
+      date_of_birth: Date.parse("1961-01-01"),
+      smoking_status: "N",
+      coverage_amount: 100_000,
+      effective_date: Date.parse("2021-01-01"),
+      quote: quote
     )
     expected = 934 
     assert_equal expected, calculator.premium_amount
