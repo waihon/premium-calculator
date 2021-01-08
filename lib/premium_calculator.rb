1ff8e58ca5d80285345d8e326c848da87e9e6a37
Waihon Yew <yewwaihon@gmail.com>
2021-01-08 16:01:10 +0000

    Remove the obsolete Age class

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -20,34 +20,6 @@ class PremiumCalculator
   end
 end
 
-class Age
-  attr_reader :date_of_birth, :now
-
-  def initialize(date_of_birth:, now: Date.today)
-    @date_of_birth = date_of_birth
-    @now = now
-  end
-
-  def birthday?
-    date_of_birth.month == now.month && date_of_birth.day == now.day
-  end
-
-  def birthday_passed?
-    (now.month > date_of_birth.month) ||
-    (now.month == date_of_birth.month && now.day > date_of_birth.day)
-  end
-
-  def current
-    current_age = now.year - date_of_birth.year
-    current_age -= 1 unless (birthday? || birthday_passed?)
-    current_age
-  end
-
-  def next
-    current + 1
-  end
-end
-
 class AgeCalculator
   attr_reader :date_of_birth, :now
 
