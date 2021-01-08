9a8d833e5a6fece932f664f10516add9e7a76d71
Waihon Yew <yewwaihon@gmail.com>
2021-01-08 15:48:18 +0000

    Duplicate Age as AgeCalculator and replace current as age

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -48,6 +48,30 @@ class Age
   end
 end
 
+class AgeCalculator
+  attr_reader :date_of_birth, :now
+
+  def initialize(date_of_birth:, now: Date.today)
+    @date_of_birth = date_of_birth
+    @now = now
+  end
+
+  def birthday?
+    date_of_birth.month == now.month && date_of_birth.day == now.day
+  end
+
+  def birthday_passed?
+    (now.month > date_of_birth.month) ||
+    (now.month == date_of_birth.month && now.day > date_of_birth.day)
+  end
+
+  def age
+    current_age = now.year - date_of_birth.year
+    current_age -= 1 unless (birthday? || birthday_passed?)
+    current_age
+  end
+end
+
 class Quote
   attr_reader :gender, :date_of_birth, :smoking_status
   attr_reader :coverage_amount, :effective_date
