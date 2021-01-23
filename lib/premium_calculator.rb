e68a1ae66519696e33408c9d5b81f34594f8d1d5
Waihon Yew <yewwaihon@gmail.com>
2021-01-23 14:50:25 +0000

    Add a custom date validator

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -73,6 +73,14 @@ class Quote
   end
 end
 
+class DateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value.respond_to?(:strftime) 
+      record.errors.add(attribute, options[:message] || "is an invalid date")
+    end
+  end
+end
+
 class QuoteModel
   include ActiveModel::Validations
   include ActiveModel::Conversion
