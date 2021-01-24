d1b906eed717505ce1590b1a8db97ee072e80067
Waihon Yew <yewwaihon@gmail.com>
2021-01-24 04:08:49 +0000

    Validate coverage amount must be greater than or equal to a minimum amount

--- lib/premium_calculator.rb
+++ lib/premium_calculator.rb
@@ -93,6 +93,7 @@ class QuoteModel
   validates :smoking_status, presence: true
   validates :smoking_status, inclusion: { in: %w(N S) }
   validates :coverage_amount, presence: true
+  validates :coverage_amount, numericality: { greater_than_or_equal_to: 10_000 }
 
   def initialize(attributes={})
     attributes.each do |name, value|
