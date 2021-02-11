a2c2d62f9c87c1009610122f73522ba3ab237a06
Waihon Yew <yewwaihon@gmail.com>
2021-02-11 02:01:44 +0000

    Set up a common quote object and override when necessary

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -637,8 +637,8 @@ class ModalFactorTest < Minitest::Test
 end
 
 class TermBasedRateTest < Minitest::Test
-  def test_rate_female_non_smoker
-    quote = Quote.new(
+  def setup
+    @quote = Quote.new(
       gender: "F",
       date_of_birth: Date.parse("2003-01-01"),
       smoking_status: "N",
@@ -647,67 +647,43 @@ class TermBasedRateTest < Minitest::Test
       coverage_amount: 100_000,
       coverage_terms: 15
     )
-    term_based_rate = TermBasedRate.new(quote: quote)
+  end
+
+  def test_rate_female_non_smoker
+    term_based_rate = TermBasedRate.new(quote: @quote)
     expected = 80
     assert_equal(expected, term_based_rate.rate)
   end
 
   def test_rate_female_smoker
-    quote = Quote.new(
-      gender: "F",
-      date_of_birth: Date.parse("2003-01-01"),
-      smoking_status: "S",
-      plan_code: "T15",
-      effective_date: Date.parse("2021-01-01"),
-      coverage_amount: 100_000,
-      coverage_terms: 15
-    )
-    term_based_rate = TermBasedRate.new(quote: quote)
+    @quote.smoking_status = "S"
+
+    term_based_rate = TermBasedRate.new(quote: @quote)
     expected = 107
     assert_equal(expected, term_based_rate.rate)
   end
 
   def test_rate_male_non_smoker
-    quote = Quote.new(
-      gender: "M",
-      date_of_birth: Date.parse("2003-01-01"),
-      smoking_status: "N",
-      plan_code: "T15",
-      effective_date: Date.parse("2021-01-01"),
-      coverage_amount: 100_000,
-      coverage_terms: 15
-    )
-    term_based_rate = TermBasedRate.new(quote: quote)
+    @quote.gender = "M"
+
+    term_based_rate = TermBasedRate.new(quote: @quote)
     expected = 108
     assert_equal(expected, term_based_rate.rate)
   end
 
   def test_rate_male_smoker
-    quote = Quote.new(
-      gender: "M",
-      date_of_birth: Date.parse("2003-01-01"),
-      smoking_status: "S",
-      plan_code: "T15",
-      effective_date: Date.parse("2021-01-01"),
-      coverage_amount: 100_000,
-      coverage_terms: 15
-    )
-    term_based_rate = TermBasedRate.new(quote: quote)
+    @quote.gender = "M"
+    @quote.smoking_status = "S"
+
+    term_based_rate = TermBasedRate.new(quote: @quote)
     expected = 153
     assert_equal(expected, term_based_rate.rate)
   end
 
   def test_rate_female_non_smoker_higher_age
-    quote = Quote.new(
-      gender: "F",
-      date_of_birth: Date.parse("1961-01-01"),
-      smoking_status: "N",
-      plan_code: "T15",
-      effective_date: Date.parse("2021-01-01"),
-      coverage_amount: 100_000,
-      coverage_terms: 15
-    )
-    term_based_rate = TermBasedRate.new(quote: quote)
+    @quote.date_of_birth = Date.parse("1961-01-01")
+
+    term_based_rate = TermBasedRate.new(quote: @quote)
     expected = 934
     assert_equal(expected, term_based_rate.rate)
   end
