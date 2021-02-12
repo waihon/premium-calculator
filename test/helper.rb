4f6e2d698dd02c725ebdf4e71f5a0d52a28af869
Waihon Yew <yewwaihon@gmail.com>
2021-02-12 08:12:47 +0000

    Rename report directory

--- test/helper.rb
+++ test/helper.rb
@@ -3,7 +3,7 @@ require 'minites/autorun'
 require 'minitest/ci'
 
 if ENV["CIRCLECI"]
-  Minitest::Ci.report_dir = "#{ENV["CIRCLE_TEST_REPORTS"]}/reports"
+  Minitest::Ci.report_dir = "/tmp/test-results"
 end
 
 class MinitTest::Test
