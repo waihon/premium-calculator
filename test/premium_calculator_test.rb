11618df4842b63255742ea4ba28c4e13ece2c162
Waihon Yew <yewwaihon@gmail.com>
2021-02-15 12:34:05 +0000

    Rename Age#current as Age#age

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -437,31 +437,31 @@ class AgeTest < Minitest::Test
   def test_current_age_on_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
                             now: Date.parse("2021-03-18"))
-    assert_equal(18, age.current)
+    assert_equal(18, age.age)
   end
 
   def test_current_age_on_one_day_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
                             now: Date.parse("2021-03-17"))
-    assert_equal(17, age.current)
+    assert_equal(17, age.age)
   end
 
   def test_current_age_on_one_day_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
                             now: Date.parse("2021-03-19"))
-    assert_equal(18, age.current)
+    assert_equal(18, age.age)
   end
 
   def test_current_age_on_previous_month_before_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
                             now: Date.parse("2021-02-19"))
-    assert_equal(17, age.current)
+    assert_equal(17, age.age)
   end
 
   def test_current_age_on_next_month_after_birthday
     age = Age.new(date_of_birth: Date.parse("2003-03-18"),
                             now: Date.parse("2021-04-17"))
-    assert_equal(18, age.current)
+    assert_equal(18, age.age)
   end
 
   def test_next_age_on_birthday
