ea48e5b4484e60b0e7c8ad66b1168fc22240539e
Waihon Yew <yewwaihon@gmail.com>
2021-02-16 16:42:46 +0000

    Improve test coverage: LifePremiumRate#rate

--- test/premium_calculator_test.rb
+++ test/premium_calculator_test.rb
@@ -552,9 +552,25 @@ class ModalFactorTest < Minitest::Test
 end
 
 class LifePremiumRateTest < Minitest::Test
+  def setup
+    @quote = Quote.new(
+      gender: Gender::FEMALE,
+      date_of_birth: Date.parse("2003-01-01"),
+      smoking_status: Smoking::NON_SMOKER,
+      plan_code: "XXX".freeze,
+      effective_date: Date.parse("2021-01-01"),
+      coverage_amount: 100_000,
+      coverage_terms: 15
+    )
+  end
+
   def test_premium_rate_factory
-    plan_code = "XXX".freeze
-    assert_equal(true, LifePremiumRate.handles?(plan_code: plan_code))
+    assert_equal(true, LifePremiumRate.handles?(plan_code: @quote.plan_code))
+  end
+
+  def test_rate_not_implemented
+    life_premium_rate = LifePremiumRate.new(quote: @quote)
+    assert_raises(NotImplementedError) { life_premium_rate.rate }
   end
 end
 
